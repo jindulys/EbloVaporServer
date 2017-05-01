0ca7e0bc818416e04031f64806e9adc6f7903d47
jindulys <liyansong.edw@gmail.com>
2017-04-30 22:13:40 -0400

    Add in EnvironmentManger.

---
+++ Sources/App/Models/EnvironmentManager.swift
@@ -0,0 +1,14 @@
+//
+//  EnvironmentManager.swift
+//  EbloVaporServer
+//
+//  Created by yansong li on 2017-04-30.
+//
+//
+
+import Foundation
+
+/// A manager that is responsible for controlling the environment of this app.
+struct EnviromentManager {
+  static let local: Bool = false
+}
